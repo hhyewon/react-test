4de255aee1bc525345cbc9518ac8a6c7008feea6
hiimhyewon <HWUHLyVper5mDbGxBqXPyM3omwE1DOT+Z/UNy8izQ70=>
2023-08-02 17:07:39 +0900

    hook 전까지

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1>hello world</h1>
+        <FuncComp initNumber={2}/>
+        <ClassComp initNumber={2}/>
+        <HookComp initNumber={2}/>
     </div>
   );
+}
+
+// function >> return (간단하게 사용)
+function FuncComp(props) {
+  return (
+    <div className="container">
+      <h2>function style component</h2>
+        <p>Number : {props.initNumber}</p>
+    </div>
+  );
+}
+
+// class >> 메소드를 정의해야 함 (다양한 기능)
+class ClassComp extends React.Component {
+    // state 초기화
+    state = {
+        number: this.props.initNumber
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h2>class style component</h2>
+                {/* state 값 사용 */}
+                <p>Number : {this.state.number}</p>
+                <input type="button" value="random" onClick={
+                    function () {
+                        this.setState({number:Math.random()});
+                    }.bind(this)
+                }/>
+            </div>
+        );
+    }
+}
+
+
+// hook >> function 방식에서 class를 사용할 수 있게 됨.
+function HookComp(props) {
+    var numberState = useState();
+    console.log(numberState)
+
+
+    return (
+        <div className="container">
+            <h2>function style component</h2>
+            <p>Number : {props.initNumber}</p>
+        </div>
+    );
 }
 
 export default App;
